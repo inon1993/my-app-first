c8e5d3d496d14c7e230243970324deae3c2abe02
inon1993 <inon1993@gmail.com>
2022-02-08 01:12:19 +0200

    finish validation and check if user exist

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,43 +1,54 @@
-import Card from '../UI/Card'
-import SignUpForm from './SignUpForm';
-import axios from 'axios';
-import classes from './SignUp.module.css';
+import { useState } from "react";
+import Card from "../UI/Card";
+import SignUpForm from "./SignUpForm";
+import axios from "axios";
+import classes from "./SignUp.module.css";
 
 const SignUp = (props) => {
+  const [isUsernameExist, setIsUsernameExist] = useState(false);
 
-    const addNewUserHandler = (fname, lname, email, username, password) => {
-        const payload = {
-            fname: fname,
-            lname: lname,
-            email: email,
-            username: username,
-            password: password,
-          };
-      
-          axios({
-            url: "/api/signup",
-            method: "POST",
-            data: payload,
-          })
-            .then(() => {
-              console.log("Data has been sent to the server.");
-              
-            })
-            .catch(() => {
-              console.log("Internal server error.");
-            });
-    }
+  const addNewUserHandler = (fname, lname, email, username, password) => {
+    const payload = {
+      fname: fname,
+      lname: lname,
+      email: email,
+      username: username,
+      password: password,
+    };
 
-    const closeSignUpHandler = () => {
-        props.onClose();
-    }
+    axios({
+      url: "/api/signup",
+      method: "POST",
+      data: payload,
+    })
+      .then((response) => {
+        if (response.data.msg === "Username already exists") {
+          setIsUsernameExist(true);
+        } else {
+          setIsUsernameExist(false);
+          console.log("Data has been sent to the server.");
+        }
+      })
+      .catch(() => {
+        console.log("Internal server error.");
+      });
+  };
+
+  const closeSignUpHandler = () => {
+    props.onClose();
+  };
 
   return (
-    <div >
-      <Card className={classes['sign-up-card']}>
-      <h1 className={classes['sign-up-hello']}>Lets Sign You Up!</h1>
-      <SignUpForm onLoginForm={props.onLogin} onClose={closeSignUpHandler} onSignUp={addNewUserHandler} />
-    </Card>
+    <div>
+      <Card className={classes["sign-up-card"]}>
+        <h1 className={classes["sign-up-hello"]}>Lets Sign You Up!</h1>
+        <SignUpForm
+          onLoginForm={props.onLogin}
+          onClose={closeSignUpHandler}
+          onSignUp={addNewUserHandler}
+          unExist={isUsernameExist}
+        />
+      </Card>
     </div>
   );
 };
